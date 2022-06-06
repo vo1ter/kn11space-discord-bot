a2736515709383d5a38f0182ec673722879d877f
Maxim <45738898+vo1ter@users.noreply.github.com>
2022-06-06 14:15:11 +0100

    Commented command builder for dev server

--- deploy-commands.js
+++ deploy-commands.js
@@ -20,10 +20,11 @@ const rest = new REST({ version: '9' }).setToken(token);
 	try {
 		console.log('Started refreshing application (/) commands.');
 
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, devGuildId),
-			{ body: commands },
-		);
+		// await rest.put(
+		// 	Routes.applicationGuildCommands(clientId, devGuildId),
+		// 	{ body: commands },
+		// );
+		// uncomment this section if u need commands only on your dev server
 
 		await rest.put(
 			Routes.applicationCommands(clientId),
